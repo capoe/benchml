da93297deebe8acaec1ac15f4f8ceb33469538e5
capoe <carlpoelking@gmail.com>
2020-09-15 16:35:05 +0100

    Endpoint=kernel.

--- examples/extract/extract_kernel.py
+++ examples/extract/extract_kernel.py
@@ -2,7 +2,7 @@ import benchml as bml
 
 if __name__ == "__main__":
     bml.log.Connect()
-    bml.log.AddArg("models", (list,str), default="^bmol_ecfp_krr$")
+    bml.log.AddArg("models", (list,str), default=["^bmol_ecfp_krr$"])
     bml.log.AddArg("meta_json", str, default="../../test/test_data/molecular/meta.json")
     args = bml.log.Parse()
 
@@ -13,10 +13,10 @@ if __name__ == "__main__":
     for data in datasets:
         for model in models:
             stream = model.open(data)
-            model.precompute(stream)
             for train, test in stream.split(method="random", n_splits=1, train_fraction=100./len(data)):
-                model.fit(train) # .fit rather than .map because some descriptors need to be "fitted"
+                model.fit(train, endpoint="kernel") # .fit rather than .map because some descriptors need to be "fitted"
                 K = train.resolve('kernel.K')
                 print(K)
                 print(K.shape)
+                input('< voila, the kernel')
 
