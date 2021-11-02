7d1ec59c8ff8dd46631bbf0e89c03f17a4bccdf8
Rudolfs Petrovs <rudolfs.petrovs@astx.com>
2021-11-02 19:06:16 +0000

    Refactored duplicate code to get_bench_pdf_soap_krr_kwargs

--- benchml/models/mod_bench.py
+++ benchml/models/mod_bench.py
@@ -2,7 +2,13 @@ import numpy as np
 
 import benchml.transforms as btf
 from benchml.hyper import GridHyper, Hyper
-from benchml.models.common import get_bench_pdf_gylm_rr_kwargs, make_gylm_krr, make_gylm_rr
+from benchml.models.common import (
+    get_bench_pdf_gylm_krr_kwargs,
+    get_bench_pdf_gylm_rr_kwargs,
+    get_bench_pdf_soap_krr_kwargs,
+    make_gylm_krr,
+    make_gylm_rr,
+)
 
 whiten_hyper = [False]  # NOTE: False = no whitening in ridge models
 regularization_range = np.logspace(-9, +7, 17)
@@ -857,60 +863,7 @@ def make_pdf_krr(minimal):
         ),
         btf.Module(
             tag="bmol_pdf_gylm_%s_krr" % ("minimal" if minimal else "standard"),
-            transforms=[
-                btf.ExtXyzInput(tag="input"),
-                btf.GylmAtomic(
-                    tag="descriptor_atomic",
-                    args={
-                        "rcut": 3.0 if minimal else 5.0,
-                        "rcut_width": 0.5,
-                        "nmax": 6 if minimal else 9,
-                        "lmax": 4 if minimal else 6,
-                        "sigma": 0.75,
-                        "part_sigma": 0.5,
-                        "wconstant": False,
-                        "wscale": 0.5,
-                        "wcentre": 0.5,
-                        "ldamp": 0.5,
-                        "power": False,
-                        "normalize": False,
-                    },
-                    inputs={"configs": "input.configs"},
-                ),
-                btf.GylmReduceConvolve(
-                    tag="descriptor",
-                    args={
-                        "nmax": "@descriptor_atomic.nmax",
-                        "lmax": "@descriptor_atomic.lmax",
-                        "types": "@descriptor_atomic.types",
-                        "normalize": True,  # NOTE Important
-                    },
-                    inputs={"Q": "descriptor_atomic.X"},
-                ),
-                btf.KernelDot(tag="kernel", inputs={"X": "descriptor.X"}),
-                btf.DoDivideBySize(
-                    tag="input_norm",
-                    args={"config_to_size": "lambda c: len(c)", "skip_if_not_force": False},
-                    inputs={"configs": "input.configs", "meta": "input.meta", "y": "input.y"},
-                ),
-                btf.KernelRidge(
-                    tag="predictor",
-                    args={"alpha": None, "power": 2},
-                    inputs={"K": "kernel.K", "y": "input_norm.y"},
-                ),
-                btf.UndoDivideBySize(
-                    tag="output", inputs={"y": "predictor.y", "sizes": "input_norm.sizes"}
-                ),
-            ],
-            hyper=GridHyper(
-                Hyper(
-                    {
-                        "predictor.alpha": regularization_range,
-                    }
-                )
-            ),
-            broadcast={"meta": "input.meta"},
-            outputs={"y": "output.y"},
+            **get_bench_pdf_gylm_krr_kwargs(minimal, regularization_range),
         ),
     ]
 
@@ -919,58 +872,11 @@ def make_pdf_rr(minimal):
     return [
         btf.Module(
             tag="bmol_pdf_soap_%s_rr" % ("minimal" if minimal else "standard"),
-            transforms=[
-                btf.ExtXyzInput(tag="input"),
-                btf.SoapGylmxx(
-                    tag="descriptor_atomic",
-                    args={
-                        "rcut": 3.0 if minimal else 5.0,
-                        "nmax": 6 if minimal else 9,
-                        "lmax": 4 if minimal else 6,
-                        "sigma": 1.0,
-                        "types": None,
-                        "crossover": True,
-                        "periodic": None,
-                        "power": False,
-                        "normalize": False,
-                    },
-                    inputs={"configs": "input.configs"},
-                ),
-                btf.GylmReduceConvolve(
-                    tag="descriptor",
-                    args={
-                        "nmax": "@descriptor_atomic.nmax",
-                        "lmax": "@descriptor_atomic.lmax",
-                        "types": "@descriptor_atomic.types",
-                        "normalize": True,  # NOTE Important
-                    },
-                    inputs={"Q": "descriptor_atomic.X"},
-                ),
-                btf.WhitenMatrix(tag="whiten", inputs={"X": "descriptor.X"}),
-                btf.DoDivideBySize(
-                    tag="input_norm",
-                    args={"config_to_size": "lambda c: len(c)", "skip_if_not_force": False},
-                    inputs={"configs": "input.configs", "meta": "input.meta", "y": "input.y"},
-                ),
-                btf.Ridge(tag="predictor", inputs={"X": "whiten.X", "y": "input_norm.y"}),
-                btf.UndoDivideBySize(
-                    tag="output", inputs={"y": "predictor.y", "sizes": "input_norm.sizes"}
-                ),
-            ],
-            hyper=GridHyper(
-                Hyper({"whiten.centre": whiten_hyper, "whiten.scale": whiten_hyper}),
-                Hyper(
-                    {
-                        "predictor.alpha": regularization_range,
-                    }
-                ),
-            ),
-            broadcast={"meta": "input.meta"},
-            outputs={"y": "output.y"},
+            **get_bench_pdf_soap_krr_kwargs(minimal, whiten_hyper, regularization_range),
         ),
         btf.Module(
             tag="bmol_pdf_gylm_%s_rr" % ("minimal" if minimal else "standard"),
-            **get_bench_pdf_gylm_rr_kwargs(minimal, whiten_hyper, regularization_range)
+            **get_bench_pdf_gylm_rr_kwargs(minimal, whiten_hyper, regularization_range),
         ),
     ]
 
