5f2b8334f93f8ceb95c1492d7b5751ae8f27b084
Bingqing Cheng <bc509@login-e-16.data.cluster>
2021-11-11 09:49:27 +0000

    added the individual plot style

--- analysis/plot_lc.py
+++ analysis/plot_lc.py
@@ -36,7 +36,7 @@ def read_acronym(filename="bml_name_acronym.list"):
     return acronym_dict
 
 
-def plot_lc_seperate(model_category, sc_name, best_model_error, lc_by_model, lr_by_model, acronym_dict, color_dict, num_col, num_row):
+def plot_lc_category(model_category, sc_name, best_model_error, lc_by_model, lr_by_model, acronym_dict, color_dict, num_col, num_row):
     if num_row == 0:
         num_row = math.ceil(len(model_category)/num_col)
     fig, ax = plt.subplots(nrows=num_row, ncols=num_col,figsize=(3*num_col,2*num_row),sharex=True,sharey=True)
@@ -45,6 +45,8 @@ def plot_lc_seperate(model_category, sc_name, best_model_error, lc_by_model, lr_
         i = mi%(num_col*num_row)
         category = v['category']
         best_model = v['model']
+        print(i, v)
+
         if best_model is None: continue
 
         if num_row > 1 and num_col > 1: # 2D subplot
@@ -73,6 +75,39 @@ def plot_lc_seperate(model_category, sc_name, best_model_error, lc_by_model, lr_
             ax_now.set_ylabel('Test {}'.format(sc_name),labelpad=-2)
 
     return fig, ax, ax0
+
+def plot_lc_individual(model_category, sc_name, best_model_error, lc_by_model, lr_by_model, acronym_dict, color_dict, num_col, num_row):
+    fig, ax = plt.subplots(nrows=num_row, ncols=num_col,figsize=(3*num_col,1*num_row),sharex=True,sharey=True)
+
+    for mi, [_, v] in enumerate(dict(sorted(best_model_error.items())).items()):
+        category = v['category']
+        best_model = v['model']
+        if best_model is None: continue
+
+        for mj, key_now in enumerate(model_category[category]):
+            if lc_by_model[key_now] is None: continue
+            #print(mj, mi, category, key_now)
+            if mj >= num_row:
+                print("WARNING: more models in the category " + category + " than the number of rows.")
+                continue
+            ax_now = ax[mj,mi]
+            #for key in model_category[category]:
+            #    ax_now.errorbar(np.asarray(lc_by_model[key])[:,0], np.asarray(lc_by_model[key])[:,1], yerr=np.asarray(lc_by_model[key])[:,2],
+            #        linestyle='-', c=color_dict[category], alpha=0.5,
+            #        uplims=True, lolims=True)
+
+            ax_now.errorbar(np.asarray(lc_by_model[key_now])[:,0], np.asarray(lc_by_model[key_now])[:,1], yerr=np.asarray(lc_by_model[key_now])[:,2],
+                    linestyle='-',linewidth=4, c=color_dict[category], alpha=1.0,
+                    label=acronym_dict[key_now]+"\n"+"LR="+"{:.1e}".format(lr_by_model[key_now]),
+                    uplims=True, lolims=True)
+
+            ax_now.legend(loc='best') # bbox_to_anchor=(1.3, 0.5))
+            if mj == (num_row -1):
+                ax_now.set_xlabel('N',labelpad=-3)
+            if mi == 0:
+                ax_now.set_ylabel('Test {}'.format(sc_name),labelpad=-2)
+
+    return fig, ax, ax[0,0]
     
 def plot_lc_together(model_category, sc_name, best_model_error, lc_by_model, lr_by_model, acronym_dict, color_dict):
     fig, ax = plt.subplots(figsize=(8,6))
@@ -98,7 +133,7 @@ def plot_lc_together(model_category, sc_name, best_model_error, lc_by_model, lr_
     ax.set_ylabel('Test {}'.format(sc_name),labelpad=-5)
     return fig, ax, ax
 
-def main(filename, prefix, sc_name, ncol, nrow):
+def main(filename, prefix, plot_type, sc_name, ncol, nrow):
 
     by_model = parse(filename)
     all_model_keys = list(by_model.keys())
@@ -115,21 +150,28 @@ def main(filename, prefix, sc_name, ncol, nrow):
     lc_by_model = {}
     #lc_by_model_train = {}
     lr_by_model = {}
-    best_model_error = {}
+
+    best_error = 10**20. # just for setting the range of the plot
     worst_error = 0.0
-    n_category = 0
-    # compute the learning curves and get the best model in each catagory
+
+    sorted_model_category = {}
+    best_model_error = {}
+
+    # compute the learning curves and sort the models in each catagory
     for mi, [category, model_key] in enumerate(model_category.items()):
-        best_error = 10**20.
-        best_model = None
+        sorted_model_category[category] = {}
+
         for key_now in model_key:
             try:
                 lc_by_model[key_now], _, _ = get_learning_curve(by_model, model_key_now=key_now,
                                                        sc_name=sc_name)
                 #lc_by_model[key_now], lc_by_model_train[key_now], _ = get_learning_curve(by_model, model_key_now=key_now,
                 #                                       sc_name=sc_name)
+
+                sorted_model_category[category][key_now] = np.min(lc_by_model[key_now][:,1])
+
                 if np.min(lc_by_model[key_now][:,1]) < best_error:
-                    best_error, best_model = np.min(lc_by_model[key_now][:,1]), key_now
+                    best_error = np.min(lc_by_model[key_now][:,1])
                 if lc_by_model[key_now][0,1] > worst_error:
                     worst_error = lc_by_model[key_now][0,1]
 
@@ -139,25 +181,37 @@ def main(filename, prefix, sc_name, ncol, nrow):
             except:
                 lc_by_model[key_now] = None
                 continue
-        if best_model is not None: n_category += 1
-        best_model_error[best_error] = { "category": category, "model": best_model}
 
+        try:
+            # sort the models in each category
+            sorted_model_category[category] = dict(sorted(sorted_model_category[category].items(), key=lambda item: item[1]))
+            #print(sorted_model_category[category])
+            [ best_in_catogory, best_in_catogory_error ] = next(iter( sorted_model_category[category].items() ))
+            best_model_error[best_in_catogory_error] = { "category": category, "model": best_in_catogory}
+        except:
+            continue
+
+
+    # dumpy the data for the learning curves and learning rates
     test_RMSE = [ [k, lc_by_model[k][-1,1]] for k in by_model.keys() if lc_by_model[key_now] is not None]
     np.savetxt(prefix+'-test_RMSE.dat', test_RMSE, fmt='%s')
     with open(prefix+'-lc.json', 'w') as jd:
         json.dump(lc_by_model,jd, sort_keys=False, cls=NumpyEncoder)
 
     # plot
-    if ncol*nrow == 1:
-        fig, ax, ax0 = plot_lc_together(model_category, sc_name, best_model_error, lc_by_model, lr_by_model, acronym_dict, color_dict)
+    if plot_type == 'together':
+        fig, ax, ax0 = plot_lc_together(sorted_model_category, sc_name, best_model_error, lc_by_model, lr_by_model, acronym_dict, color_dict)
+    elif plot_type == 'category':
+        fig, ax, ax0 = plot_lc_category(sorted_model_category, sc_name, best_model_error, lc_by_model, lr_by_model, acronym_dict, color_dict, ncol, nrow)
+    elif plot_type == 'individual':
+        fig, ax, ax0 = plot_lc_individual(sorted_model_category, sc_name, best_model_error, lc_by_model, lr_by_model, acronym_dict, color_dict, ncol, nrow)
     else:
-        # if ncol > n_category: ncol = n_category
-        fig, ax, ax0 = plot_lc_seperate(model_category, sc_name, best_model_error, lc_by_model, lr_by_model, acronym_dict, color_dict, ncol, math.ceil(n_category/ncol))
+        raise ValueError("plot type not recognized.")
 
     ax0.set_ylim([best_error*0.7,worst_error*1.3])
     ax0.set_xscale('log')
     ax0.set_yscale('log')
-    ax0.text(0.2, 0.95, prefix, fontsize=14,
+    ax0.text(0.2, 0.9, prefix, fontsize=14,
          horizontalalignment='left',
          verticalalignment='center',
          transform = ax0.transAxes)
@@ -168,9 +222,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=None)
     parser.add_argument('-f', '--file', type = str, default = 'bml', help = 'Location of the .json.gz file.')
     parser.add_argument('-p', '--prefix', type = str, default = 'bml', help = 'Prefix of the output file.')
+    parser.add_argument('-t', '--type', type = str, default = 'category', help = 'Type for plotting the learning curves: [together/category/individual]')
     parser.add_argument('-m', '--metric', type = str, default = 'RMSE', help = 'The metric for plotting the learning curves.')
     parser.add_argument('-c', '--column', type = int, default = 4, help = 'The number of columns')
     parser.add_argument('-r', '--row', type = int, default = 0, help = 'The number of rows')
     args = parser.parse_args()
-    sys.exit(main(args.file, args.prefix, args.metric, args.column, args.row))
+    sys.exit(main(args.file, args.prefix, args.type, args.metric, args.column, args.row))
 
